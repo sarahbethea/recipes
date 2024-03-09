30f772392e81afda58d91009130134234b6e9e33
Sarah Bethea <sarahbethea@sarahs-mbp.lan>
2024-03-08 17:45:11 -0700

    first commit

---
+++ src/main/java/recipes/dao/DbConnection.java
@@ -0,0 +1,35 @@
+package recipes.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import recipes.exception.DbException;
+
+public class DbConnection {
+	
+	private static final String SCHEMA = "recipes";
+	private static final String USER = "recipes";
+	private static final String PASSWORD = "recipes";
+	private static final String HOST = "localhost";
+	private static final int PORT = 3306;
+	
+	
+	public static Connection getConnection() {
+		String url = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&", HOST, PORT, SCHEMA,
+				USER, PASSWORD);
+		
+
+		
+		try {
+			Connection conn = DriverManager.getConnection(url);
+			System.out.println("Connection successful!");
+			return conn;
+		} catch (SQLException e) {
+			throw new DbException(e);
+			
+		}
+		
+	}
+
+}
